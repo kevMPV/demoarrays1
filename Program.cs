4e9d0008546acdbe82fd26d60d18770ae12e89a5
eduardo <eduardo18mnc@gmail.com>
2026-05-16 21:09:25 +0000

    cree un arreglo y accedi a sus elementos

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Progran 
+{
+    static void Main ()
+    {
+      int[] notas = {81, 92, 78, 96, 61};
+
+      for (int i=0; i < notas.Length; i ++)
+      {
+        Console.WriteLine($"Posicion [{i}]: {notas[i]}");
+      }
+    }
+} 
